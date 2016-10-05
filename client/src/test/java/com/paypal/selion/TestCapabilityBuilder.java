a6bdb85139f8da28c42edf88d50d2bb286003edc
Sherif Ebady <sebady@paypal.com>
2016-10-05 10:29:30 -0500

    Add brower build targets to travis
    - Add additional build targets for travis CI to also run our
      chrome and firefox suites locally.
    - Also moved the "deploy" step to phantom target.
    - Reduced the number of threads in some test suites
    - Added a custom test capabilityprovider for setting browser options
    - Need to add "--no-sandbox" for chrome execution to work around
      chrome on OpenVZ.
    - Also add firefox in travis CI using Firefox-suite
    - Other minor test updates
    - Keep all temporary test files in target folder

---
+++ client/src/test/java/com/paypal/selion/TestCapabilityBuilder.java
@@ -0,0 +1,60 @@
+/*-------------------------------------------------------------------------------------------------------------------*\
+|  Copyright (C) 2016 PayPal                                                                                          |
+|                                                                                                                     |
+|  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance     |
+|  with the License.                                                                                                  |
+|                                                                                                                     |
+|  You may obtain a copy of the License at                                                                            |
+|                                                                                                                     |
+|       http://www.apache.org/licenses/LICENSE-2.0                                                                    |
+|                                                                                                                     |
+|  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed   |
+|  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for  |
+|  the specific language governing permissions and limitations under the License.                                     |
+\*-------------------------------------------------------------------------------------------------------------------*/
+
+package com.paypal.selion;
+
+import com.paypal.selion.configuration.Config;
+import com.paypal.selion.configuration.Config.ConfigProperty;
+import com.paypal.selion.internal.platform.grid.BrowserFlavors;
+import com.paypal.selion.internal.platform.grid.WebTestSession;
+import com.paypal.selion.platform.grid.Grid;
+import com.paypal.selion.platform.grid.browsercapabilities.DefaultCapabilitiesBuilder;
+import org.apache.commons.lang3.StringUtils;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * A custom capabilitiesBuilder used during test executions to establish custom browser paths and options.
+ */
+public class TestCapabilityBuilder extends DefaultCapabilitiesBuilder {
+
+    @Override
+    public DesiredCapabilities getCapabilities(DesiredCapabilities capabilities) {
+        String browserPath = System.getProperty("BROWSER_PATH");
+        WebTestSession session = Grid.getWebTestSession();
+        if (StringUtils.isEmpty(browserPath) || session == null) {
+            return capabilities;
+        }
+
+        String browser = session.getBrowser();
+        if (browser.equals(BrowserFlavors.CHROME.getBrowser())) {
+            ChromeOptions options = new ChromeOptions();
+            options.setBinary(browserPath);
+            // To run chrome on virtualized openVZ environments
+            options.addArguments("--no-sandbox");
+            capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+        } else if (browser.equals(BrowserFlavors.FIREFOX.getBrowser())) {
+            Map<String, String> firefoxOptions = new HashMap<>();
+            firefoxOptions.put("binary", browserPath);
+            String key = Config.getBoolConfigProperty(ConfigProperty.SELENIUM_USE_GECKODRIVER) ?
+                "moz:firefoxOptions" : "firefox_binary";
+            capabilities.setCapability(key, firefoxOptions);
+        }
+        return capabilities;
+    }
+}
